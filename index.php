554a8d0ad09fee5af683301ce272b0306e98bcfe
sashko <112473214+cofficcc@users.noreply.github.com>
2023-12-08 08:58:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,23 +20,3 @@ session_start();
 
 $router = new Router;
 $router->run();
-//phpscan_auth_token
-//337|O1MZsDIpKcpDfs2i3JMiEtqI28ZCzZRDbZP9doed5985a50a
-
-name: GitHub Actions PhpScan
-on:
-  push:
-    branches:
-      - master
-      - main
-jobs:
-  Phpscan-Action:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout
-        uses: actions/checkout@v3
-      - name: Phpscan Vulnerabilities Scanner
-        uses: phpscan/phpscan_action@v0.1.7
-        env:
-          PROJECT_NAME: "CI/CD Real Test"
-          PHPSCAN_AUTH_TOKEN: ${{ secrets.PHPSCAN_AUTH_TOKEN }}
